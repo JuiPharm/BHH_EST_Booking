c484858118042a90849642f91cc954e2a86b963f
JuiPharm <jui_3065@hotmail.com>
2026-08-25 17:19:36 +0700

    Update API_URL to point to the live endpoint

--- web/config.js
+++ web/config.js
@@ -1,3 +1,3 @@
 window.EST_CONFIG = Object.freeze({
-  API_URL: 'YOUR_APPS_SCRIPT_WEB_APP_URL/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbz0xQCVH1p7Zw3apgRl77iR7Qn6X7h5u4PUFl0OCL7sbzF7mgIgxHN5KH6kPO2_08WN/exec'
 });
